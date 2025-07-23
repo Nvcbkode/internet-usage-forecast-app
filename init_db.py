a20c227818bc231a72ba4d484847f0cac0eac7a3
Michael Olushola Olugbenga <163161261+Nvcbkode@users.noreply.github.com>
2025-07-23 13:59:59 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -4,9 +4,9 @@ import bcrypt
 
 def connect():
     return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        dbname=os.getenv("DB_NAME", "forecast_app"),
+        user=os.getenv("DB_USER", "postgres"),
+        password=os.getenv("DB_PASSWORD", "yourpassword"),
+        host=os.getenv("DB_HOST", "localhost"),
+        port=os.getenv("DB_PORT", "5432")
     )
